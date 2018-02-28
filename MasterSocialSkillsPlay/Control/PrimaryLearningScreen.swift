0ddd55b10ed4ba56325d956296e9fc1ca7d16109
user133107 <user133107@103-246-99-72.moboware.com>
2018-03-01 09:03:49 +1100

    displaying question and answer text on learning screen. changed learning screen to call music class to play the music

--- MasterSocialSkillsPlay/Control/PrimaryLearningScreen.swift
+++ MasterSocialSkillsPlay/Control/PrimaryLearningScreen.swift
@@ -7,22 +7,21 @@
 ////
 //
 import UIKit
-import AVFoundation
+//import AVFoundation
 
 class primaryLearningScreen: UIViewController {
     
     var count = 0
-    var audioPlayer : AVAudioPlayer!
+//    var audioPlayer : AVAudioPlayer!
     var questionState : Bool = true
     var sceneSelection = ""
     var modeSelection = ""
     let finalArray = BuildRandomArrays()
+    var bbbbb = MakeMusicClass.init()
 
     @IBOutlet weak var modeSelected: UILabel!
     @IBOutlet weak var sceneSelected: UILabel!
-
-    
-    
+    @IBOutlet weak var displayText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,19 +36,17 @@ class primaryLearningScreen: UIViewController {
     }
     
 
-    func makeNoise(soundFileName: String) { //play the selected media play
-        print ("the  sound file name is : " + soundFileName)
-        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "mp3")
-        do {
-            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
-            audioPlayer.play()
-        }
-        catch {
-            print (error)
-        }
-    }
-    
-    @IBOutlet weak var displayText: UILabel!
+//    func makeNoise(soundFileName: String) { //play the selected media play
+//        print ("the  sound file name is : " + soundFileName)
+//        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "mp3")
+//        do {
+//            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+//            audioPlayer.play()
+//        }
+//        catch {
+//            print (error)
+//        }
+//    }
     
     @IBAction func nextquestion(_ sender: Any) {
         
@@ -58,13 +55,15 @@ class primaryLearningScreen: UIViewController {
                     if questionState == true {
                         print ("you are up to question number: \(count) = \(finalArray.list[count].questionNumber)")
                         let soundFileNameInput = sceneSelection+"Question"+String(finalArray.list[count].questionNumber)
-                        makeNoise(soundFileName: soundFileNameInput)
+                        bbbbb.makeNoise(Selection: soundFileNameInput)
+                        displayText.text = finalArray.list[count].questionText
                         questionState = !questionState
                     }
                     else if questionState == false {
                         print ("you are up to answer number: \(count) = \(finalArray.list[count].answerNumber)")
                         let soundFileNameInput = sceneSelection+"Answer"+String(finalArray.list[count].answerNumber)
-                        makeNoise(soundFileName: soundFileNameInput)
+                        bbbbb.makeNoise(Selection: soundFileNameInput)
+                        displayText.text = finalArray.list[count].answerText
                         count += 1
                         questionState = !questionState
                         }
